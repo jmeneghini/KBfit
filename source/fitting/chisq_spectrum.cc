c3ee994ed3e7d4c710564dcf65e037ed7f290448
jmeneghini <meneghinijohn@gmail.com>
2025-05-19 12:29:22 -0400

    added more blocks to K pi and started on spectrum fit

---
+++ source/fitting/chisq_spectrum.cc
@@ -0,0 +1,22 @@
+#include "chisq_spectrum.h"
+using namespace std;
+
+SpectrumFit::~SpectrumFit() {
+  clear();
+}
+
+void SpectrumFit::clear() {
+  for (uint k = 0; k < BQ.size(); ++k)
+    delete BQ[k];
+  delete Kmat;
+  delete Kinv;
+  BQ.clear();
+}
+
+void SpectrumFit::guessInitialFitParamValues(
+    vector<double>& fitparams) const {
+  if (Kmat != 0)
+    fitparams = Kmat->getParameterValues();
+  else
+    fitparams = Kinv->getParameterValues();
+}
